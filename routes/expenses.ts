bea27d2e623f5b7b1236520dba2ef2d63a9321eb
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-06-03 01:07:35 +0530

    feat(api): Implement expense creation with validation
    feat(api): Implement expense creation with validation
    
    * Introduces a POST handler for creating new expense entries.
    * Validates incoming expense data using Zod schema and `@hono/zod-validator`.
    * Defines a Zod schema (`createPostSchema`) for expense creation requests.
    * Validates title length, positive integer amount, and date format.
    * Adds new expense data to the mock data store (`fakeExpenses`).
    * Provides a basic functionality for creating expenses with data validation.
    * Updates dependencies to include `@hono/zod-validator` and `zod`.

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,39 +1,47 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { z } from "zod";
 
 type Expense = {
   id: number;
+  title: string;
   amount: number;
-  description: string;
   date: string;
 };
 
 const fakeExpenses: Expense[] = [
-  {
+ {
     id: 1,
-    amount: 1000,
-    description: "Groceries",
+    title: "Groceries",
+    amount: 100.00,
     date: "2021-01-01",
   },
   {
     id: 2,
-    amount: 2000,
-    description: "Rent",
+    title: "Rent",
+    amount: 1000.00,
     date: "2021-01-02",
   },
   {
     id: 3,
-    amount: 3000,
-    description: "Coffee",
+    title: "Gas",
+    amount: 20.00,
     date: "2021-01-03",
-  },
+  }
 ];
+
+const createPostSchema = z.object({
+  title: z.string().min(3).max(100),
+  amount: z.number().int().positive(),
+  date: z.string(),
+});
 
 export const expensesRoute = new Hono()
   .get("/", async (c) => {
     return c.json({ expenses: fakeExpenses });
   })
-  .post("/", async (c) => {
+  .post("/", zValidator("json", createPostSchema), async (c) => {
     const expense = await c.req.json();
-    console.log(expense);
+    fakeExpenses.push({...expense, id: fakeExpenses.length + 1 });
     return c.json(expense);
   });
