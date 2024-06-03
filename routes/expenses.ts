91cc13eb3b99782b8c338c25ed75c46654854368
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-06-04 02:01:02 +0530

    feat(api): Implement expense retrieval and deletion
    feat(api): Implement expense retrieval and deletion
    
    * Introduces a new GET route handler to retrieve an expense by ID (`/:id`).
    * Validates the ID parameter using a regular expression (`[0-9]+`).
    * Returns a 404 Not Found response if the expense with the specified ID is not found.
    * Adds a new DELETE route handler to delete an expense by ID (`/:id`).
    * Finds the expense index based on the ID in the URL parameter.
    * Removes the expense from the `fakeExpenses` array using `splice`.
    * Returns the deleted expense data in the JSON response.
    * Provides functionalities for retrieving and deleting expenses by ID.

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -2,46 +2,59 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { z } from "zod";
 
-type Expense = {
-  id: number;
-  title: string;
-  amount: number;
-  date: string;
-};
+
+const expenseSchema = z.object({
+  id: z.number().int().positive().min(1),
+  title: z.string().min(3).max(100),
+  amount: z.number().int().positive(),
+});
+
+type Expense = z.infer<typeof expenseSchema>;
+
+const createPostSchema = expenseSchema.omit({ id: true });
 
 const fakeExpenses: Expense[] = [
- {
+  {
     id: 1,
     title: "Groceries",
-    amount: 100.00,
-    date: "2021-01-01",
+    amount: 100,
   },
   {
     id: 2,
-    title: "Rent",
-    amount: 1000.00,
-    date: "2021-01-02",
+    title: "Rents",
+    amount: 1000,
   },
   {
     id: 3,
-    title: "Gas",
-    amount: 20.00,
-    date: "2021-01-03",
-  }
+    title: "Gass",
+    amount: 200,
+  },
 ];
-
-const createPostSchema = z.object({
-  title: z.string().min(3).max(100),
-  amount: z.number().int().positive(),
-  date: z.string(),
-});
 
 export const expensesRoute = new Hono()
   .get("/", async (c) => {
     return c.json({ expenses: fakeExpenses });
   })
   .post("/", zValidator("json", createPostSchema), async (c) => {
-    const expense = await c.req.json();
-    fakeExpenses.push({...expense, id: fakeExpenses.length + 1 });
+    const expense = await c.req.valid("json");
+    fakeExpenses.push({ ...expense, id: fakeExpenses.length + 1 });
     return c.json(expense);
+  })
+  .get("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+    const expense = fakeExpenses.find((expense) => expense.id === id);
+    if (!expense) {
+      return c.notFound();
+    }
+    return c.json({ expense });
+  })
+  .delete("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+    const index = fakeExpenses.findIndex((expense) => expense.id === id);
+    if (index === -1) {
+      return c.notFound();
+    }
+
+    const deletedExpense = fakeExpenses.splice(index, 1)[0];
+    return c.json({ expense: deletedExpense });
   });
